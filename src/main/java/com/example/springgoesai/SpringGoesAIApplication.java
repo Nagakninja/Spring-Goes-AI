5cab0ddd114577b95e353bea6b8d23c3988d57a9
Nagakninja <104618429+Nagakninja@users.noreply.github.com>
2024-05-03 16:20:43 +0530

    Initial AI ChatClient with ChatController endpoint : /api/generate generating a dad joke (#1)

---
+++ src/main/java/com/example/springgoesai/SpringGoesAIApplication.java
@@ -0,0 +1,13 @@
+package com.example.springgoesai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGoesAIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringgoesaiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springgoesai/SpringGoesAIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springgoesai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGoesAIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
