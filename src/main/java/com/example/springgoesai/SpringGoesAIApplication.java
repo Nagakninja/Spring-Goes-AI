1b18a100a6b1beb1780e202814b96b69b4bf2bc8
Nagakninja <nagak789@gmail.com>
2024-05-03 16:45:52 +0530

    Code Refactoring and clean up changes

--- src/main/java/com/example/springgoesai/SpringGoesAIApplication.java
+++ src/main/java/com/example/springgoesai/SpringGoesAIApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringGoesAIApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringgoesaiApplication.class, args);
+		SpringApplication.run(SpringGoesAIApplication.class, args);
 	}
 
 }
